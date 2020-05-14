542ad355255b24b8edb40947fe08c78b324bcc9a
samuelskinstad <s_skinstad@hotmail.com>
2020-05-14 11:25:47 +0200

    added stuff in ui and logic

--- FitnessAB/src/fitnessab/Logic.java
+++ FitnessAB/src/fitnessab/Logic.java
@@ -13,4 +13,11 @@ public class Logic {
     public void addMember(String namn, String eNamn, int pNr){
         //TODO SQL add member
     }
+    public void memberLookup(int id){
+        //TODO view member information
+        System.out.println(id);
+    }
+    public void viewGym(int id){
+        //TODO view gym information
+    }
 }
--- FitnessAB/src/fitnessab/UI.java
+++ FitnessAB/src/fitnessab/UI.java
@@ -13,19 +13,38 @@ import java.util.Scanner;
  */
 public class UI {
     Scanner scan = new Scanner(System.in);
-    Logic logic;
+    Logic logic = new Logic();
+    boolean quit = false;
     public void UI(){
-        System.out.println("1: lägg till ny medlem");
-        String input = scan.nextLine();
-        switch(input){
-            case "1":
-                System.out.println("Namn:");
-                String namn = scan.nextLine();
-                System.out.println("Efternamn:");
-                String eNamn = scan.nextLine();
-                System.out.println("PNummer (ÅÅMMDD):");
-                int pNr = scan.nextInt();
-                logic.addMember(namn, eNamn, pNr);
+        while(!quit){
+            System.out.println("1: lägg till ny medlem\n"
+                    + "2: Information om medlem\n"
+                    + "3: Avsluta");
+            String input = scan.nextLine();
+            switch(input){
+                case "1":
+                    System.out.println("Namn:");
+                    String namn = scan.nextLine();
+                    System.out.println("Efternamn:");
+                    String eNamn = scan.nextLine();
+                    System.out.println("PNummer (ÅÅMMDD):");
+                    int pNr = scan.nextInt();
+                    logic.addMember(namn, eNamn, pNr);
+                    break;
+                    
+                case "2":
+                    System.out.println("MedlemsID:");
+                    int id = scan.nextInt();
+                    logic.memberLookup(id);
+                    break;
+                    
+                case "3":
+                    quit = true;
+                    
+                default:
+                    System.out.println("Felaktig input");
+                    break;
+            }
         }
     }
 }
